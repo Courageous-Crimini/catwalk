b1a3e18dd300c2c9711acd347100e096a6cd9b9c
jacquesdubois <jacquesadubois@gmail.com>
2021-09-09 13:31:18 -0400

    Added Webpack and Babel

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+
+const path = require('path');
+const SRC_DIR = '/client';
+const DIST_DIR = './public';
+
+module.exports = {
+  entry: path.join(__dirname, `${SRC_DIR}/index.js`),
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, DIST_DIR)
+  },
+  devtool: 'eval-source-map',
+  watch: true,
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react"
+            ],
+            plugins: [
+              ["@babel/plugin-transform-runtime",
+                {
+                  "regenerator": true
+                }
+              ]
+            ]
+          }
+        }
+      }
+    ]
+  }
+};
