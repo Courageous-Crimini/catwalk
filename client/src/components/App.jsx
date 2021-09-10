27d469fee58f3a4f261a4affe693289b4bb32434
nickwai1 <53497786+nickwai1@users.noreply.github.com>
2021-09-09 23:53:04 -0700

    changes to react boiler plate

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
-import GlobalStyle from '../globalStyles.jsx';
+import styled from 'styled-components';
 import Header from './Header/Header.jsx';
 import Overview from './Overview/Overview.jsx';
 import RelatedProducts from './RelatedProducts/RelatedProducts.jsx';
 import QA from './QA/QA.jsx';
 import Reviews from './Reviews/Reviews.jsx';
 
+const Container = styled.div`
+width: 100%;
+display: flex;
+flex-direction: column;
+justify-content: flex-start;
+align-items: center;
+font-family: Valera Round, sans-serif;
+`
+
 const App = () => {
     return (
-        <div>
+        <Container>
             <Header />
             <Overview />
-            <GlobalStyle />
             <QA/>
             <RelatedProducts/>
             <Reviews/>
-        </div>
-    )
+        </Container>
+    );
 };
 
 export default App;
