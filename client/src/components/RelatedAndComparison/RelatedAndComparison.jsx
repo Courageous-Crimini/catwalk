14ddb4819536a2130130790517d72a9a3171e2ad
Raeye Jordan <raeyejordaan@yahoo.com>
2021-09-10 18:42:26 -0700

    Added eslint with airbnb and installed jest

--- client/src/components/RelatedAndComparison/RelatedAndComparison.jsx
+++ client/src/components/RelatedAndComparison/RelatedAndComparison.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
+import RelatedProducts from './assets/RelatedProducts';
 
 const Wrapper = styled.section`
 padding: 20em;
 background: #DF9EF6;
 `;
 
-const RelatedAndComparison= () => {
-    return (
-        <Wrapper>
-            <h2> Related Products </h2>
-            <h2>Your Outfits</h2>
-        </Wrapper>
-    )
-};
+const RelatedAndComparison = () => (
+  <Wrapper>
+    <h2> Related Products </h2>
+    <RelatedProducts />
+    <h2>Your Outfits</h2>
+  </Wrapper>
+
+);
 
 export default RelatedAndComparison;
---
+++ client/src/components/RelatedAndComparison/assets/RelatedProducts.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import RelatedCard from './Card';
+
+const RelatedProducts = () => {
+  <RelatedCard />;
+};
+
+export default RelatedProducts;
