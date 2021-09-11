34f8d799496fb4d74d6899aa771204426006daec
nickwai1 <53497786+nickwai1@users.noreply.github.com>
2021-09-11 01:18:37 -0700

    get data to render through useEffect

--- client/src/components/QA/QA.jsx
+++ client/src/components/QA/QA.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 
@@ -11,6 +11,18 @@ text-align: center;
 `;
 
 const QA = () => {
+    const [productId, setProductId] = useState(48432);
+
+    useEffect(() => {
+        axios.get(`/api/qa/questions?product_id=${productId}`)
+        .then(({data}) => {
+            console.log(data);
+        })
+        .catch((err) => {
+            console.error(err);
+        })
+    }, []);
+
     return (
         <Wrapper>
             <div> 
