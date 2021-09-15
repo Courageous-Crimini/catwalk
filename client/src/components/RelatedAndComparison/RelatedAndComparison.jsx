446b95b820a247f33ad870e94475ec6db18e097c
Raeye Jordan <raeyejordaan@yahoo.com>
2021-09-14 19:42:09 -0700

    Able to get styles and render info to page

--- client/src/components/RelatedAndComparison/RelatedAndComparison.jsx
+++ client/src/components/RelatedAndComparison/RelatedAndComparison.jsx
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import styled from 'styled-components';
+import RelatedProducts from './assets/RelatedProducts.jsx';
+// import YourOutfit from './assets/YourOutfit.jsx';
 
-const Wrapper = styled.section`
-margin-top: 15em;
-padding: 20em;
-background: #DF9EF6;`;
+const RelatedAndComparison = () => {
+  const [relatedIds, setRelatedIds] = useState([]);
+  const [styles, setStyle] = useState([]);
 
-const RelatedAndComparison = () => (
-  <Wrapper>
-    <h2>Related Products</h2>
-  </Wrapper>
-);
+  // const [yourOutfit, setYourOutfit] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/products/48432/related')
+      .then(({ data }) => {
+        setRelatedIds(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    const stylesData = relatedIds.map((item) => axios.get(`/api/products/${item}/styles`)
+      .then(({ data }) => {
+        // get images
+        console.log('Here is the data', data);
+        return data;
+      }));
+    Promise.all(stylesData)
+      .then((values) => {
+        console.log('Here are the values in RC', values);
+        setStyle(values);
+      });
+  }, [relatedIds]);
+
+  return (
+    <div id="RelatedAndComparison">
+      <h2>Related Products</h2>
+      <ul>
+        <RelatedProducts styles={styles} />
+      </ul>
+      <h2>Your Outfit</h2>
+      {/* <YourOutfit /> */}
+    </div>
+  );
+};
 
 export default RelatedAndComparison;
---
+++ client/src/components/RelatedAndComparison/assets/RelatedProducts.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const RelatedProducts = ({ styles }) => {
+  console.log('Styles:', styles);
+
+  const cards = styles.map((item) => {
+    const id = item.product_id;
+    const image = item.results[0].photos.url;
+    // const category = ;
+    const name = item.results[0].name;
+    const price = item.results[0].sale_price || item.results[0].original_price;
+
+    console.log(image);
+
+    return (
+      <div className="card" key={id}>
+        <img src={image} alt="A related produce" />
+        <button type="button" className="relatedBtn">&#9733;</button>
+        <br />
+        <span>Category</span>
+        <br />
+        <span>{name}</span>
+        <br />
+        <span>{price}</span>
+      </div>
+    );
+  });
+
+  return (
+    <div className="carousel">
+      {cards}
+    </div>
+  );
+};
+
+export default RelatedProducts;
