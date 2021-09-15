b0a1f7fd3279afb82e912ca1a8ea3fb27f418cd0
Raeye Jordan <raeyejordaan@yahoo.com>
2021-09-14 19:51:12 -0700

    Able to get styles and render info to page

--- client/src/components/RelatedAndComparison/assets/RelatedProducts.jsx
+++ client/src/components/RelatedAndComparison/assets/RelatedProducts.jsx
@@ -5,7 +5,7 @@ const RelatedProducts = ({ styles }) => {
 
   const cards = styles.map((item) => {
     const id = item.product_id;
-    const image = item.results[0].photos.url;
+    const image = item.results[0].photos.thumbnail_url;
     // const category = ;
     const name = item.results[0].name;
     const price = item.results[0].sale_price || item.results[0].original_price;
