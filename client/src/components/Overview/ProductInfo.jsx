72a9fa25ef64810d8f5e7145b5f16e51d8997406
jacquesdubois <jacquesadubois@gmail.com>
2021-09-17 12:59:20 -0400

    Refactored ProductInfo to dynamically render sale prices, if applicable

--- client/src/components/Overview/ProductInfo.jsx
+++ client/src/components/Overview/ProductInfo.jsx
@@ -16,8 +16,66 @@ padding: 3% 5%;
 const ProductInfo = () => {
   const state = useContext(StateContext);
 
-  const productInfo = state.products.filter((product) => product.id === state.selectedProduct);
+  const productInfo = state.products.filter((product) => product.id === state.selectedProduct)[0];
+  const styleInfo = state.styles.filter((style) => style.style_id === state.selectedStyle)[0];
 
+  if (styleInfo.sale_price) {
+    return (
+      <Wrapper style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'left',
+        alignContent: 'center',
+        justifyContent: 'space-evenly',
+      }}
+      >
+        <span style={{
+          order: '1',
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          alignContent: 'center',
+          justifyContent: 'space-between',
+        }}
+        >
+          <p style={{ fontSize: '1.25em', margin: '0', marginRight: '10%' }}>
+            &#9734; &#9734; &#9734; &#9734; &#9734;
+          </p>
+          <p style={{ margin: '0' }}><u>Read all reviews</u></p>
+        </span>
+        <p style={{
+          order: '2',
+          margin: '0',
+          fontWeight: 'lighter',
+        }}
+        >
+          {productInfo.category}
+        </p>
+        <h4 style={{
+          order: '3',
+          margin: '0',
+          fontSize: '1.75em',
+          fontWeight: 'bolder',
+        }}
+        >
+          {productInfo.name}
+        </h4>
+        <p style={{
+          order: '4',
+          margin: '0',
+          fontSize: '1.25em',
+        }}
+        >
+          <span>
+            <p style={{ display: 'inline', textDecoration: 'line-through' }}>{`$${styleInfo.original_price}`}</p>
+            <p style={{ marginLeft: '10px', display: 'inline', color: 'green' }}>
+              {styleInfo.sale_price ? `SALE: $${styleInfo.sale_price}` : ''}
+            </p>
+          </span>
+        </p>
+      </Wrapper>
+    );
+  }
   return (
     <Wrapper style={{
       display: 'flex',
@@ -33,7 +91,6 @@ const ProductInfo = () => {
         flexDirection: 'row',
         alignItems: 'center',
         alignContent: 'center',
-        // margin: '0 5%',
         justifyContent: 'space-between',
       }}
       >
@@ -45,31 +102,27 @@ const ProductInfo = () => {
       <p style={{
         order: '2',
         margin: '0',
-        // marginLeft: '5%',
         fontWeight: 'lighter',
       }}
       >
-        {productInfo[0].category}
+        {productInfo.category}
       </p>
       <h4 style={{
         order: '3',
         margin: '0',
-        // marginLeft: '5%',
         fontSize: '1.75em',
         fontWeight: 'bolder',
       }}
       >
-        {productInfo[0].name}
+        {productInfo.name}
       </h4>
       <p style={{
         order: '4',
         margin: '0',
-        // marginLeft: '5%',
         fontSize: '1.25em',
       }}
       >
-        $
-        {productInfo[0].default_price}
+        {`$${styleInfo.original_price}`}
       </p>
     </Wrapper>
   );
