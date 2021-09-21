86d6841b4c2271eb414dff23f5b5050a707c81ca
jacquesdubois <jacquesadubois@gmail.com>
2021-09-21 10:52:38 -0400

    Continuing to set up RTL

--- client/src/components/Overview/__tests__/AddToCart.test.jsx
+++ client/src/components/Overview/__tests__/AddToCart.test.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import AddToCart from '../AddToCart.jsx';
 
 test('On initial render, the "Add to Cart" button is disabled', () => {
   render(<AddToCart />);
 
-  screen.debug();
+  // expect(screen.getByDisplayValue('Add To Cart')).toBeDisabled();
+});
+
+test('The "Add to Cart" button is enabled after a user selects a valid size and quantity', () => {
+  render(<AddToCart />);
+  // userEvent.select(screen.getByValue('Select Size', 'L'))
+  // userEvent.select(screen.getByValue('Select Quantity', '1'))
+  // expect(screen.getByDisplayValue('Add To Cart')).toBeEnabled();
 });
