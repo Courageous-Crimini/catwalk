14ccd786a6c7529952725f9ddc65a35a44561c66
Raeye Jordan <raeyejordaan@yahoo.com>
2021-09-23 08:00:25 -0700

    Deleted unnecessary imports

--- client/src/components/RelatedAndComparison/__tests__/RelatedAndComparison.test.jsx
+++ client/src/components/RelatedAndComparison/__tests__/RelatedAndComparison.test.jsx
@@ -1,13 +1,10 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 import { render, screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import { StateContext, DispatchContext, initialState } from '../../App.jsx';
 import RelatedAndComparison from '../RelatedAndComparison.jsx';
-// import { RelatedContext } from '../Context.jsx';
-// import state from './requests.jsx';
 
 const server = setupServer(
   rest.get('/greeting', (req, res, ctx) => res(ctx.json({ greeting: 'hello there' }))),
